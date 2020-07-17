d3eb94f1462fe010d9182a924055580bc24a5783
Joon Jang <w.joon.jang@gmail.com>
2020-07-16 17:47:28 -0700

    Add project files.

---
+++ GoogleSpreadsheetDemo/GoogleSpreadsheetDemo/DataArrayModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleSpreadsheetDemo
+{
+    public class DataArrayModel
+    {
+        public string[][] DataArray{ get; set; }
+    }
+}
---
+++ GoogleSpreadsheetDemo/GoogleSpreadsheetDemo/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GoogleSpreadsheetDemo
+{
+    //https://github.com/saamerm/Xamarin-GoogleSheetsDB
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        string[][] setArray;
+       
+        public MainPage()
+        {
+            //listToArray = new List<string>();
+            setArray = new string[5][];
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+
+        async void SubmitButton_Pressed(System.Object sender, System.EventArgs e)
+        {
+            var client = new HttpClient();
+
+
+            var model = new DataArrayModel()
+            {
+                DataArray = setArray
+            };
+
+            var uri = "https://script.google.com/macros/s/AKfycby2BGbNJwvzgqp4hay1CR0V3cznlND4u3Ra2-mysvdELCbO3II/exec";
+            var jsonString = JsonConvert.SerializeObject(model);
+
+
+            ////
+            //string tmp = "{\"DataArray\":[[\"Week0\",\"DAY 1\"],[\"69\",\"69\",\"420\",\"420\",\"42069\",\"6969\"],[\"69\",\"69\",\"420\",\"420\",\"42069\",\"6969\"],[\"69\",\"69\",\"420\",\"420\",\"42069\",\"6969\"],[\"69\",\"69\",\"420\",\"420\",\"42069\",\"6969\"]]}";
+            //var requestContent = new StringContent(tmp);
+            ////
+
+            var requestContent = new StringContent(jsonString);
+
+            var result = await client.PostAsync(uri, requestContent);
+            var resultContent = await result.Content.ReadAsStringAsync();
+
+            ResponseModel response = null;
+            try
+            {
+                response = JsonConvert.DeserializeObject<ResponseModel>(resultContent);
+            }
+            catch { }
+
+            ProcessResponse(response);
+        }
+
+        private void ProcessResponse(ResponseModel responseModel)
+        {
+            ResultLabel.IsVisible = true;
+            ResultLabel.Text = responseModel.Message;
+            if (responseModel.Status == "SUCCESS")
+                ResultLabel.TextColor = Color.Black;
+            else
+                ResultLabel.TextColor = Color.Red;
+        }
+
+        private void ArrayButton_Pressed(object sender, EventArgs e)
+        {
+            if(this.setArray[0] is null)
+            {
+                this.setArray[0] = new string[] { WeekEntry.Text, DayEntry.Text };
+            }
+
+           
+
+           this.setArray[arrayIteration] = new string[] 
+            {
+                SetOneEntry.Text,
+                SetTwoEntry.Text,
+                SetThreeEntry.Text,
+                SetFourEntry.Text,
+                SetFiveEntry.Text,
+                SetSixEntry.Text
+            };
+
+            ArrayIteration++;
+        }
+
+        private int arrayIteration = 1;
+        public int ArrayIteration
+        {
+            get { return arrayIteration; }
+            set
+            {
+                arrayIteration = value;
+                OnPropertyChanged(nameof(ArrayIteration));
+            }
+        }
+    }
+}
